a7648711b51998c7e4d2bc0cc6946a578f244993
Quynhlethi <lequynh94.hrm.neu@gmail.com>
2018-06-15 11:38:27 +0700

    draw in the stupid way

--- src/LuyenTap/ThietKeMenuChoUngDung.java
+++ src/LuyenTap/ThietKeMenuChoUngDung.java
@@ -7,13 +7,16 @@ public class ThietKeMenuChoUngDung {
         int choice = -1;
         Scanner input = new Scanner(System.in);
 
-       while (choice != 0) { System.out.println("Menu");
+       while (choice != 0) {
+           System.out.println("Menu");
            System.out.println("1. Draw the triangle");
            System.out.println("2. Draw the square");
            System.out.println("3. Draw the rectangle");
            System.out.println("0. Exit");
            System.out.println("Enter your choice: ");
-           choice = input.nextInt();switch (choice) {
+
+           choice = input.nextInt();
+           switch (choice) {
                case 1:
                    System.out.println("Draw the triangle");
                    System.out.println("******");
